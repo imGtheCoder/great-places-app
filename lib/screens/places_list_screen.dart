773a8227f7ab95f0b5cd85be2cf0a65fa1f553cd
imGtheCoder <stiopimanol34@gmail.com>
2023-02-08 20:07:57 +0200

    Using SQLite, saving Places on local storage

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -20,26 +20,35 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: const Text('Got no places yet, start adding some!'),
-        ),
-        builder: (context, greatPlaces, child_whichDoesntChange) =>
-            greatPlaces.items.length <= 0
-                ? child_whichDoesntChange
-                : ListView.builder(
-                    itemCount: greatPlaces.items.length,
-                    itemBuilder: (context, index) => ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage:
-                            FileImage(greatPlaces.items[index].image),
-                      ),
-                      title: Text(greatPlaces.items[index].title),
-                      onTap: () {
-                        //go to the detail page
-                      },
-                    ),
-                  ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (context, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: const Text('Got no places yet, start adding some!'),
+                ),
+                builder: (context, greatPlaces, child_whichDoesntChange) =>
+                    greatPlaces.items.length <= 0
+                        ? child_whichDoesntChange
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (context, index) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(greatPlaces.items[index].image),
+                              ),
+                              title: Text(greatPlaces.items[index].title),
+                              onTap: () {
+                                //go to the detail page
+                              },
+                            ),
+                          ),
+              ),
       ),
     );
   }
